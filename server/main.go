b53339356eab504e82cfc3540107c8b2dfb23e6e
emahiro <ema.hiro@gmail.com>
2023-07-08 03:46:54 +0900

    add: service template

--- server/main.go
+++ server/main.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/bufbuild/connect-go"
+	qrurlv1 "github.com/emahiro/qrurl/server/gen/proto/qrurl/v1"
+)
+
+type QrUrlService struct{}
+
+func (s *QrUrlService) PostCode(
+	ctx context.Context,
+	req *connect.Request[qrurlv1.PostCodeRequest],
+) {
+}
 
 func main() {
 	fmt.Println("Hello, World!")

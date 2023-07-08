fcb90f72a7b77765a5a12dd8013c60905fc1d715
emahiro <ema.hiro@gmail.com>
2023-07-08 17:07:08 +0900

    add: reading qr-code image

--- server/service.go/qrurl_service.go
+++ server/service.go/qrurl_service.go
@@ -1,9 +1,15 @@
 package service
 
 import (
+	"bytes"
 	"context"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/qrcode"
 
 	qrurlv1 "github.com/emahiro/qrurl/server/gen/proto/qrurl/v1"
 )
@@ -14,9 +20,28 @@ func (s *QrUrlService) PostCode(
 	ctx context.Context,
 	req *connect.Request[qrurlv1.PostCodeRequest],
 ) (resp *connect.Response[qrurlv1.PostCodeResponse], err error) {
-	qrurlResp := &qrurlv1.PostCodeResponse{
-		Url: "test",
+
+	buf := bytes.NewBuffer(req.Msg.Image)
+	img, _, err := image.Decode(buf)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
 	}
+
+	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	reader := qrcode.NewQRCodeReader()
+	result, err := reader.Decode(bmp, nil)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+
+	qrurlResp := &qrurlv1.PostCodeResponse{
+		Url: result.GetText(),
+	}
+
 	resp = connect.NewResponse(qrurlResp)
 	return resp, nil
 }

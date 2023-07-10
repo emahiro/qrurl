21442304cd10c65098d4808477fd9ce04199679f
emahiro <ema.hiro@gmail.com>
2023-07-11 03:09:11 +0900

    add: use chi router

--- server/main.go
+++ server/main.go
@@ -9,10 +9,10 @@ import (
 	"golang.org/x/exp/slog"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/go-chi/chi/v5"
 
 	"github.com/emahiro/qrurl/server/gen/proto/ping/v1/pingv1connect"
 	"github.com/emahiro/qrurl/server/gen/proto/qrurl/v1/qrurlv1connect"
-	"github.com/emahiro/qrurl/server/gen/proto/webhook/v1/webhookv1connect"
 	"github.com/emahiro/qrurl/server/intercepter"
 	"github.com/emahiro/qrurl/server/service"
 )
@@ -24,28 +24,34 @@ func main() {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	intercepters := connect.WithInterceptors(
-		intercepter.NewRequestLogIntercepter(),
-	)
-
-	mux := http.NewServeMux()
-	mux.Handle(qrurlv1connect.NewQrUrlServiceHandler(&service.QrUrlService{}, intercepters))
-	mux.Handle(pingv1connect.NewPingServiceHandler(&service.PingService{}, intercepters))
-	mux.Handle(webhookv1connect.NewLineWebhookServiceHandler(&service.LineWebhookService{}, intercepters))
-	mux.HandleFunc("/v1/webhook/line", func(w http.ResponseWriter, r *http.Request) {
-		v := map[string]any{}
-		if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
-			slog.ErrorCtx(ctx, "request body parse error", err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		slog.InfoCtx(r.Context(), "request body", "body", fmt.Sprintf("%+v", v))
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("ok"))
+	router := chi.NewRouter()
+	// 標準の http handler
+	router.Group(func(r chi.Router) {
+		r.HandleFunc("/v1/webhook/line", func(w http.ResponseWriter, r *http.Request) {
+			v := map[string]any{}
+			if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+				slog.ErrorCtx(ctx, "request body parse error", err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			slog.InfoCtx(r.Context(), "request body", "body", fmt.Sprintf("%+v", v))
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("ok"))
+		})
 	})
+
+	// for GRPC
+	router.Group(func(r chi.Router) {
+		intercepters := connect.WithInterceptors(
+			intercepter.NewRequestLogIntercepter(),
+		)
+		r.Handle(qrurlv1connect.NewQrUrlServiceHandler(&service.QrUrlService{}, intercepters))
+		r.Handle(pingv1connect.NewPingServiceHandler(&service.PingService{}, intercepters))
+	})
+
 	server := &http.Server{
 		Addr:    addr,
-		Handler: mux,
+		Handler: router,
 	}
 	go func() {
 		<-ctx.Done()

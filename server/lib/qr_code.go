e6d80121c34dd01c804202c13d31eb50ac1f0c24
emahiro <ema.hiro@gmail.com>
2023-07-11 03:32:00 +0900

    add: lib package

---
+++ server/lib/qr_code.go
@@ -0,0 +1,35 @@
+package lib
+
+import (
+	"bytes"
+	"context"
+	"image"
+
+	"github.com/bufbuild/connect-go"
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/qrcode"
+	"golang.org/x/exp/slog"
+)
+
+func DecodeQrCode(ctx context.Context, b []byte) (string, error) {
+	buf := bytes.NewBuffer(b)
+	img, _, err := image.Decode(buf)
+	if err != nil {
+		slog.ErrorCtx(ctx, "image.Decode", "err", err)
+		return "", connect.NewError(connect.CodeInvalidArgument, err)
+	}
+
+	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+	if err != nil {
+		slog.ErrorCtx(ctx, "NewBinaryBitmapFromImage", "err", err)
+		return "", connect.NewError(connect.CodeUnknown, err)
+	}
+
+	reader := qrcode.NewQRCodeReader()
+	result, err := reader.Decode(bmp, nil)
+	if err != nil {
+		slog.ErrorCtx(ctx, "reader.Decode", "err", err)
+		return "", connect.NewError(connect.CodeUnknown, err)
+	}
+	return result.GetText(), nil
+}

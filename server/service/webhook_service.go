a24579812b66f032b7ac3a33a6cf46ad882e0866
emahiro <ema.hiro@gmail.com>
2023-07-11 01:31:23 +0900

    add: log

--- server/service/webhook_service.go
+++ server/service/webhook_service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/bufbuild/connect-go"
+	"golang.org/x/exp/slog"
 
 	linewebhookv1 "github.com/emahiro/qrurl/server/gen/proto/webhook/v1"
 )
@@ -11,6 +13,8 @@ import (
 type LineWebhookService struct{}
 
 func (s *LineWebhookService) LineWebhook(ctx context.Context, req *connect.Request[linewebhookv1.LineWebhookRequest]) (*connect.Response[linewebhookv1.LineWebhookResponse], error) {
+	body := req.Msg
+	slog.InfoCtx(ctx, "request", fmt.Sprintf("%v", body))
 	resp := &linewebhookv1.LineWebhookResponse{
 		Text: "Ok",
 	}

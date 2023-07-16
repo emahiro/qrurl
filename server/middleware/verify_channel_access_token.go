0106a00b06f2af0536ae3aff22515f513f633278
emahiro <ema.hiro@gmail.com>
2023-07-17 00:55:29 +0900

    add: set default http middleware

--- server/middleware/verify_channel_access_token.go
+++ server/middleware/verify_channel_access_token.go
@@ -10,37 +10,34 @@ import (
 )
 
 // VerifyChannelAccessToken checks if channel access token is valid and if invalid fetch new token and new client.
-func VerifyChannelAccessToken() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, origReq *http.Request) {
-			ctx := origReq.Context()
-			repo := repository.LineChannelAccessTokenRepository{}
-			at, err := repo.GetLatestAccessToken(ctx)
+func VerifyChannelAccessToken(http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, origReq *http.Request) {
+		ctx := origReq.Context()
+		repo := repository.LineChannelAccessTokenRepository{}
+		at, err := repo.GetLatestAccessToken(ctx)
+		if err != nil {
+			slog.ErrorCtx(ctx, "failed to fetch latest access token: %v", "err=", err)
+			http.Error(w, "failed to fetch latest access token", http.StatusInternalServerError)
+			return
+		}
+		if at != "" {
+			valid, err := line.CheckIfTokenValid(ctx, at)
 			if err != nil {
-				slog.ErrorCtx(ctx, "failed to fetch latest access token: %v", "err=", err)
-				http.Error(w, "failed to fetch latest access token", http.StatusInternalServerError)
+				slog.ErrorCtx(ctx, "failed to check if token is valid: %v", "err=", err)
+				http.Error(w, "failed to check if token is valid", http.StatusInternalServerError)
 				return
 			}
-			if at != "" {
-				valid, err := line.CheckIfTokenValid(ctx, at)
+			if !valid {
+				at, err := line.PostChannelAccessToken(ctx)
 				if err != nil {
-					slog.ErrorCtx(ctx, "failed to check if token is valid: %v", "err=", err)
-					http.Error(w, "failed to check if token is valid", http.StatusInternalServerError)
-					return
+					slog.ErrorCtx(ctx, "failed to fetch new channel access token: %v", "err=", err)
+					http.Error(w, "failed to fetch new channel access token", http.StatusInternalServerError)
 				}
-				if !valid {
-					at, err := line.PostChannelAccessToken(ctx)
-					if err != nil {
-						slog.ErrorCtx(ctx, "failed to fetch new channel access token: %v", "err=", err)
-						http.Error(w, "failed to fetch new channel access token", http.StatusInternalServerError)
-					}
-					if err := line.NewBotClient(at); err != nil {
-						slog.ErrorCtx(ctx, "failed to fetch new bot client: %v", "err=", err)
-						panic(err)
-					}
+				if err := line.NewBotClient(at); err != nil {
+					slog.ErrorCtx(ctx, "failed to fetch new bot client: %v", "err=", err)
+					panic(err)
 				}
 			}
-			next.ServeHTTP(w, origReq)
-		})
-	}
+		}
+	})
 }
--- server/middleware/verify_line_intercepter.go
+++ server/middleware/verify_line_intercepter.go
@@ -12,35 +12,32 @@ import (
 
 var lineMessageChannelSecret = os.Getenv("LINE_MESSAGE_CHANNEL_SECRET")
 
-func VerifyLine() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, origReq *http.Request) {
-			ctx := origReq.Context()
+func VerifyLine(http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, origReq *http.Request) {
+		ctx := origReq.Context()
 
-			copyReq := origReq.Clone(ctx)
-			b, err := io.ReadAll(copyReq.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			defer copyReq.Body.Close()
+		copyReq := origReq.Clone(ctx)
+		b, err := io.ReadAll(copyReq.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		defer copyReq.Body.Close()
 
-			signagure := copyReq.Header.Get("x-line-signature")
-			decoded, err := base64.StdEncoding.DecodeString(signagure)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			hash := hmac.New(sha256.New, []byte(lineMessageChannelSecret))
-			hash.Write(b)
-			verified := hmac.Equal(decoded, hash.Sum(nil))
-			if !verified {
-				w.WriteHeader(http.StatusUnauthorized)
-				return
-			}
+		signagure := copyReq.Header.Get("x-line-signature")
+		decoded, err := base64.StdEncoding.DecodeString(signagure)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		hash := hmac.New(sha256.New, []byte(lineMessageChannelSecret))
+		hash.Write(b)
+		verified := hmac.Equal(decoded, hash.Sum(nil))
+		if !verified {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
 
-			origReq.Body = io.NopCloser(bytes.NewBuffer(b))
-			next.ServeHTTP(w, origReq)
-		})
-	}
+		origReq.Body = io.NopCloser(bytes.NewBuffer(b))
+	})
 }

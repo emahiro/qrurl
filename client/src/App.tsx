84220fc02eb31133bff5e5b07b5791578ce53476
emahiro <ema.hiro@gmail.com>
2023-07-21 01:52:25 +0900

    fix: refactor ping service

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,11 @@
 import "./App.css";
+import { Ping } from "./libs/apis/ping";
 
 function App() {
+  void Ping().then((res) => {
+    console.log(res);
+  });
+
   return (
     <>
       <h1>QRコードリーダー</h1>

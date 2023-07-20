49b1e43d0daeca41e4786b411cc5554752c86f66
emahiro <ema.hiro@gmail.com>
2023-07-21 02:08:34 +0900

    add: mode condition

--- client/src/libs/apis/transport.ts
+++ client/src/libs/apis/transport.ts
@@ -2,5 +2,8 @@ import { createConnectTransport } from "@bufbuild/connect-web";
 
 // init gRPC Client
 export const transport = createConnectTransport({
-  baseUrl: process.env.TRANSPORT_URL || "http://localhost:3000",
+  baseUrl:
+    process.env.NODE_ENV === "production"
+      ? process.env.TRANSPORT_URL || ""
+      : "http://localhost:3000" || "",
 });

ce2f34179cba259cc1a83d1e90649cdf1dc93c1c
emahiro <ema.hiro@gmail.com>
2023-07-21 04:29:20 +0900

    fix: debug log

--- client/src/libs/apis/ping.ts
+++ client/src/libs/apis/ping.ts
@@ -5,8 +5,6 @@ import { PingService } from "../../../gen/proto/ping/v1/ping_connectweb";
 const client = createPromiseClient(PingService, transport);
 
 export const Ping = async () => {
-  console.log(import.meta.env.VITE_TRANSPORT_URL);
-
   const resp = await client.ping({ message: "Hello" }, {});
   return resp;
 };

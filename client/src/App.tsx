ca4807800b7ab12bc4e7b482679a339e5516eb88
emahiro <ema.hiro@gmail.com>
2023-07-21 15:10:41 +0900

    add: convert image file to byte array

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,26 @@ function App() {
     console.log(res);
   });
 
+  const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files?.length === 0 || files === null) {
+      return;
+    }
+    const file = files[0];
+    console.log(file);
+
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = (e) => {
+      const imageBinary = e.target?.result;
+      console.log(imageBinary);
+    };
+  };
+
   return (
     <>
       <h1>QRコードリーダー</h1>
-      <p>test</p>
+      <input type="file" accept="image/*" onChange={(e) => handleImage(e)} />
     </>
   );
 }

d135e38bbc007a27e7d1da16ce762018d13f6239
emahiro <ema.hiro@gmail.com>
2023-07-25 00:37:50 +0900

    add: sample log entry

--- server/lib/log/logger.go
+++ server/lib/log/logger.go
@@ -2,7 +2,9 @@ package log
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"time"
 
 	"golang.org/x/exp/slog"
 )
@@ -13,6 +15,15 @@ func New() {
 	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 }
 
-func Infof(ctx context.Context, title string, args ...any) {
-	logger.InfoCtx(ctx, title, args...)
+func Infof(ctx context.Context, format string, args ...any) {
+	now := time.Now()
+	msg := fmt.Sprintf(format, args...)
+	logger.InfoCtx(ctx, msg,
+		"severity", slog.LevelInfo,
+		"time", now.Format(time.RFC3339Nano),
+		"message", msg,
+		"insertId", "0",
+		"spanId", "0",
+		"trace", "0",
+	)
 }

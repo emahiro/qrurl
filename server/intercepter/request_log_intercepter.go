25d3c9aac8c9303477b561cc740210837a2930d9
emahiro <ema.hiro@gmail.com>
2023-07-24 23:46:50 +0900

    add: json logger

--- server/intercepter/request_log_intercepter.go
+++ server/intercepter/request_log_intercepter.go
@@ -4,13 +4,14 @@ import (
 	"context"
 
 	"github.com/bufbuild/connect-go"
-	"golang.org/x/exp/slog"
+
+	"github.com/emahiro/qrurl/server/lib/log"
 )
 
 func NewRequestLogIntercepter() connect.UnaryInterceptorFunc {
 	intercepter := func(next connect.UnaryFunc) connect.UnaryFunc {
 		return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
-			slog.InfoCtx(ctx, "request info", "method", req.HTTPMethod(), "header", req.Header())
+			log.Infof(ctx, "request info", "method", req.HTTPMethod(), "header", req.Header())
 			return next(ctx, req)
 		}
 	}
--- server/lib/log/logger.go
+++ server/lib/log/logger.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"context"
 	"os"
 
 	"golang.org/x/exp/slog"
@@ -10,4 +11,8 @@ var logger *slog.Logger
 
 func New() {
 	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+}
+
+func Infof(ctx context.Context, title string, args ...any) {
+	logger.InfoCtx(ctx, title, args...)
 }

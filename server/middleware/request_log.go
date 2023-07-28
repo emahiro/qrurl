337e3ea9c8fa02542a2df41aa756a6659427e07e
emahiro <ema.hiro@gmail.com>
2023-07-29 00:01:44 +0900

    add: span/trace id

--- server/middleware/request_log.go
+++ server/middleware/request_log.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"context"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/emahiro/qrurl/server/lib/log"
@@ -13,6 +14,18 @@ func RequestLog(next http.Handler) http.Handler {
 		ctx := r.Context()
 		requestTime := time.Now()
 		ctx = context.WithValue(ctx, log.RequestTimeKey{}, requestTime)
+
+		xcTraceCtx := r.Header.Get("X-Cloud-Trace-Context")
+		var traceID, spanID string
+		if xcTraceCtx != "" {
+			tmp := strings.Split(xcTraceCtx, "/")
+			if len(tmp) == 2 {
+				traceID = tmp[0]
+				spanID = tmp[1]
+			}
+			ctx = context.WithValue(ctx, log.TraceIDKey{}, traceID)
+			ctx = context.WithValue(ctx, log.SpanIDKey{}, spanID)
+		}
 
 		lrw := log.NewLogHTTPResponseWriter(w)
 		next.ServeHTTP(lrw, r)

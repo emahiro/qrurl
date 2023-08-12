746ce4d8e0cd328eb8cb6a0f7dc93babef1ae242
emahiro <ema.hiro@gmail.com>
2023-08-12 15:01:27 +0900

    fix: rm log

--- server/service/ping_service.go
+++ server/service/ping_service.go
@@ -2,12 +2,10 @@ package service
 
 import (
 	"context"
-	"errors"
 
 	"github.com/bufbuild/connect-go"
 
 	pingv1 "github.com/emahiro/qrurl/server/gen/proto/ping/v1"
-	"github.com/emahiro/qrurl/server/lib/log"
 )
 
 type PingService struct{}
@@ -16,8 +14,6 @@ func (s *PingService) Ping(ctx context.Context, req *connect.Request[pingv1.Ping
 	pingResp := &pingv1.PingResponse{
 		Message: "pong",
 	}
-	err := log.WithStackTracef(errors.New("test"), "test")
-	log.Infof(ctx, "err: %+v", err)
 	resp := connect.NewResponse(pingResp)
 	return resp, nil
 }
